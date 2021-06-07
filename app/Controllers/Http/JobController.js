f6715d39e2e5cbc21a8e0625e935539e2be542ec
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-06-07 08:32:12 -0300

    Implemented create method on JobController.

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -7,5 +7,24 @@ class JobController {
     const jobs = await Job.all();
     return view.render('index', { jobs: jobs.toJSON() });
   }
+
+  async create({ request, response }) {
+    try {
+      const { title, link, description } = request.body;
+      if (!title || !link || !description) {
+        throw new Error('MISSING REQUIRED INFORMATION!');
+      }
+
+      const job = new Job;
+      job.title = title;
+      job.link = link;
+      job.description = description;
+
+      await job.save();
+      await response.status(201).send({ 'CREATED': job });
+    } catch (error) {
+      await response.status(422).send({ 'ERROR': error.message });
+    }
+  }
 }
 module.exports = JobController;
