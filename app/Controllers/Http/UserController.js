7fd95758aae26d17143625356d97854db5a0f91d
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-06-07 08:23:09 -0300

    Implemented the login validator and method on UserController.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -10,6 +10,16 @@ class UserController {
     await auth.login(user);
     return response.redirect('/');
   }
+  async login({ auth, request, session, response }) {
+    const { email, password } = request.all();
+    try {
+      await auth.attempt(email, password);
+      return response.redirect('/');
+    } catch (error) {
+      session.flash({ loginError: 'Sorry, but these credentials do not work!' });
+      return response.redirect('/login');
+    }
+  }
 }
 
 module.exports = UserController;
